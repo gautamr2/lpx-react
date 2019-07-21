c5285cc44a645eb3cd342cc1343f996b86256038
Ranjeet Gautam <gautamr@vmware.com>
2019-07-21 05:12:05 +0000

    added storybook static

--- storybook/config.js
+++ storybook/config.js
@@ -1,4 +1,14 @@
-import { configure } from '@storybook/react'
+import { addParameters, configure } from '@storybook/react'
+
+addParameters({
+  options: {
+    name: 'Lookplex',
+    url: 'https://github.com/gautamr2/lpx-react',
+    isFullscreen: false,
+    showPanel: true
+    // more configuration here
+  }
+})
 
 function loadStories () {
   require('../stories/index.js')
